2f775f6e7019e3d33188e0be314e612a9d920529
Tytater <97707822+Tytater@users.noreply.github.com>
2022-01-17 11:32:15 -0600

    UI update

--- FPS Game/Assets/Scripts/Launcher.cs
+++ FPS Game/Assets/Scripts/Launcher.cs
@@ -39,7 +39,7 @@ public class Launcher : MonoBehaviourPunCallbacks
 
 	public override void OnJoinedLobby()
 	{
-		MenuManager.Instance.OpenMenu("title");
+		MenuManager.Instance.OpenMenu("start");
 		Debug.Log("Joined Lobby");
 	}
 
